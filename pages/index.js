0a2eb0fdcbe52c871f076dfc473e83233de8a394
SarahMLim <smlim@cps.edu>
2021-01-05 20:31:50 -0600

    This is the Admin screen kind of

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,65 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import { makeStyles } from "@material-ui/core/styles"
-import Button from "@material-ui/core/Button";
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
+import DragHandleIcon from '@material-ui/icons/DragHandle';
 
-export default function App() {
+const useStyles = makeStyles((theme) => ({
+    root: {
+    '& > *': {
+      margin: theme.spacing(0),
+    },
+  },
+
+}));
+
+export default function ContainedButtons() {
   const classes = useStyles();
+
   return (
-    <Button variant="contained" color="primary">
-      Primary
-    </Button>
-  );
+   
+    <div className={classes.root}>
+   <Grid container spacing={2} direction="row" alignItems="center">
   
+          <FiberManualRecordIcon style={{ color: "green" }} />
+          <DragHandleIcon style={{ color: "black" }} />
+          <h4>Completed on time</h4>
+          
+          <FiberManualRecordIcon style={{ color: "#ffc107" }} /> 
+          <DragHandleIcon style={{ color: "black" }} />
+          <h4>Completed late</h4>
+
+          <FiberManualRecordIcon style={{ color: "red" }} /> 
+          <DragHandleIcon style={{ color: "black" }} />
+          <h4>Not completed</h4>
+    
+          <Grid item xs={7}>
+
+        
+         
+          <Button variant="contained" color="primary">
+         Go to Analytics screen
+          </Button>
+
+          <Button variant="contained" color="primary">
+         Log out
+          </Button>
+
+          </Grid>
+          </Grid>
+     
+</div>
+  
+
+  );
 }
