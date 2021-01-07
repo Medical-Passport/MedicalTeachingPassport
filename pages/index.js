0364bef075d6799c33ae07bd9d362ac873207aff
SarahMLim <smlim@cps.edu>
2021-01-06 22:21:53 -0600

    Admin screen

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
@@ -11,8 +10,6 @@ import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
-import DragHandleIcon from '@material-ui/icons/DragHandle';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -31,22 +28,10 @@ export default function ContainedButtons() {
     <div className={classes.root}>
    <Grid container spacing={2} direction="row" alignItems="center">
   
-          <FiberManualRecordIcon style={{ color: "green" }} />
-          <DragHandleIcon style={{ color: "black" }} />
-          <h4>Completed on time</h4>
-          
-          <FiberManualRecordIcon style={{ color: "#ffc107" }} /> 
-          <DragHandleIcon style={{ color: "black" }} />
-          <h4>Completed late</h4>
-
-          <FiberManualRecordIcon style={{ color: "red" }} /> 
-          <DragHandleIcon style={{ color: "black" }} />
-          <h4>Not completed</h4>
-    
+      
           <Grid item xs={7}>
 
-        
-         
+                
           <Button variant="contained" color="primary">
          Go to Analytics screen
           </Button>
