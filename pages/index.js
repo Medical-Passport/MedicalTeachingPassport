1aa46f76a5622cc748ad37254c3cfb0ab0390ef8
SarahMLim <smlim@cps.edu>
2021-01-07 16:47:39 -0600

    test

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,151 @@
 import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import Typography from '@material-ui/core/Typography';
+import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
-import DragHandleIcon from '@material-ui/icons/DragHandle';
+import Box from '@material-ui/core/Box';
+import Collapse from '@material-ui/core/Collapse';
+import IconButton from '@material-ui/core/IconButton';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
+import Button from '@material-ui/core/Button';
 
-const useStyles = makeStyles((theme) => ({
-    root: {
+const useRowStyles = makeStyles({
+  root: {
     '& > *': {
-      margin: theme.spacing(0),
+      borderBottom: 'unset',
     },
   },
 
-}));
+});
 
-export default function ContainedButtons() {
-  const classes = useStyles();
+function createData(name, calories, fat, carbs, protein, price) {
+  return {
+    name,
+    calories,
+    fat,
+    carbs,
+    protein,
+    price,
+    history: [
+      { date: '2020-01-05', customerId: '11091700', amount: 3 },
+      { date: '2020-01-02', customerId: 'Anonymous', amount: 1 },
+    ],
+  };
+}
+
+function Row(props) {
+  const { row } = props;
+  const [open, setOpen] = React.useState(false);
+  const classes = useRowStyles();
+
 
   return (
-   
-    <div className={classes.root}>
-   <Grid container spacing={2} direction="row" alignItems="center">
-  
-          <FiberManualRecordIcon style={{ color: "green" }} />
-          <DragHandleIcon style={{ color: "black" }} />
-          <h4>Completed on time</h4>
-          
-          <FiberManualRecordIcon style={{ color: "#ffc107" }} /> 
-          <DragHandleIcon style={{ color: "black" }} />
-          <h4>Completed late</h4>
+    <React.Fragment>
+      <TableRow className={classes.root}>
+        <TableCell>
+          <IconButton aria-label="expand row" size="small" onClick={() => setOpen(!open)}>
+            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+          </IconButton>
+        </TableCell>
+        <TableCell component="th" scope="row">
+          {row.name}
+        </TableCell>
+        <TableCell align="right">{row.Activity1}</TableCell>
+        <TableCell align="right">{row.Activity2}</TableCell>
+        <TableCell align="right">{row.Activity3}</TableCell>
+        <TableCell align="right">{row.Activity4}</TableCell>
+      </TableRow>
+      <TableRow>
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <Box margin={1}>
+              <Typography variant="h6" gutterBottom component="div">
+                Goals
+              </Typography>
+              <Table size="small" aria-label="purchases">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Date</TableCell>
+                    <TableCell>Goal</TableCell>
+                   
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {row.history.map((historyRow) => (
+                    <TableRow key={historyRow.date}>
+                      <TableCell component="th" scope="row">
+                        {historyRow.date}
+                      </TableCell>
+                      <TableCell>{historyRow.customerId}</TableCell>
+                      <TableCell align="right">{historyRow.amount}</TableCell>
+                      <TableCell align="right">
+                        {Math.round(historyRow.amount * row.price * 100) / 100}
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </Box>
+          </Collapse>
+        </TableCell>
+      </TableRow>
+    </React.Fragment>
+  );
+}
 
-          <FiberManualRecordIcon style={{ color: "red" }} /> 
-          <DragHandleIcon style={{ color: "black" }} />
-          <h4>Not completed</h4>
-    
-          <Grid item xs={7}>
+Row.propTypes = {
+  row: PropTypes.shape({
+    Activity1: PropTypes.number.isRequired,
+    Activity2: PropTypes.number.isRequired,
+    Activity3: PropTypes.number.isRequired,
+    Activity4: PropTypes.arrayOf(
+      PropTypes.shape({
+        amount: PropTypes.number.isRequired,
+        customerId: PropTypes.string.isRequired,
+        date: PropTypes.string.isRequired,
+      }),
+    ).isRequired,
+    name: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    protein: PropTypes.number.isRequired,
+  }).isRequired,
+};
 
-        
-         
-          <Button variant="contained" color="primary">
-         Go to Analytics screen
-          </Button>
+const rows = [
+  createData('Name', 159, 6.0, 24, 4.0, 3.99),
+  createData('Name', 50, 9.0, 37, 4.3, 4.99),
 
-          <Button variant="contained" color="primary">
-         Log out
-          </Button>
+];
 
-          </Grid>
-          </Grid>
-     
-</div>
-  
+export default function CollapsibleTable() {
+  return (
+    <TableContainer component={Paper}>
+      <Table aria-label="collapsible table">
+        <TableHead>
+          <TableRow>
+            <TableCell />
+            <TableCell>Names</TableCell>
+            <TableCell align="right">Activity1</TableCell>
+            <TableCell align="right">Activity2</TableCell>
+            <TableCell align="right">Activity3</TableCell>
+            <TableCell align="right">Activity4</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <Row key={row.name} row={row} />
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+
 
   );
 }
