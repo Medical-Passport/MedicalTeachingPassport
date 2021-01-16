255f240db59f6af09914de69fd4cb044e0697ea3
SarahMLim <smlim@cps.edu>
2021-01-16 17:18:01 -0600

    lol idk

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState} from 'react';
+import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
@@ -8,43 +9,167 @@ import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import { Select, MenuItem, FormControl, InputLabel, makeStyles } from "@material-ui/core";
+import FormHelperText from '@material-ui/core/FormHelperText';
+
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-    '& > *': {
-      margin: theme.spacing(0),
-    },
+ 
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
   },
 
+  formControl: {
+
+    minWidth: 100
+    
+  }
 }));
 
-export default function ContainedButtons() {
+export default function SignUp() {
   const classes = useStyles();
 
   return (
-   
-    <div className={classes.root}>
-   <Grid container spacing={2} direction="row" alignItems="center">
-  
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        
+        <Typography component="h1" variant="h5">
+       Edit Profile
+        </Typography>
+        <form className={classes.form} noValidate>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                autoComplete="fname"
+                name="firstName"
+                variant="outlined"
+                required
+                fullWidth
+                id="firstName"
+                label="First Name"
+                autoFocus
+            
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="lastName"
+                label="Last Name"
+                name="lastName"
+                autoComplete="lname"
+               
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+              
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              
+              />
+            </Grid>
+            <Grid container justify="center">
+              <h3>Edit your section, institution, and cohort:</h3>
+            </Grid>
+          </Grid>
+          <Grid container justify="center">
+          <FormControl className={classes.formControl}>
+        <InputLabel id="demo-simple-select-label">Section</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+
+ 
+        >
+          <MenuItem value={10}>Medicine</MenuItem>
+          
+        </Select>
+      </FormControl>
+ 
       
-          <Grid item xs={7}>
-
-                
-          <Button variant="contained" color="primary">
-         Go to Analytics screen
-          </Button>
-
-          <Button variant="contained" color="primary">
-         Log out
-          </Button>
-
-          </Grid>
-          </Grid>
+      
+      <FormControl className={classes.formControl}>
+        <InputLabel id="demo-simple-select-helper-label">Institution</InputLabel>
+        <Select
+          labelId="demo-simple-select-helper-label"
+          id="demo-simple-select-helper"
+        
+        >
+         
+          <MenuItem value={10}>UChicago</MenuItem>
+         
+        </Select>
+       
+      </FormControl>
      
-</div>
-  
+     
+      <FormControl className={classes.formControl}>
+        <InputLabel id="demo-simple-select-helper-label">Cohort</InputLabel>
+        <Select
+          labelId="demo-simple-select-helper-label"
+          id="demo-simple-select-helper"
+        
+        >
+      
+          <MenuItem value={10}>2020-2021</MenuItem>
+         
+        </Select>
+       
+      </FormControl>
 
+</Grid>
+
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Submit
+          </Button>
+          <Grid container>
+           
+          
+          </Grid>
+        </form>
+      </div>
+    </Container>
   );
 }
