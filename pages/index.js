a6df5c43bae6178518a1916b4f89b11d5459dd2a
SarahMLim <smlim@cps.edu>
2021-01-22 18:25:19 -0600

    I made a graph and included 2 buttons for emails

--- pages/index.js
+++ pages/index.js
@@ -1,160 +1,261 @@
-import React from "react";
-import { ResponsiveBar } from "@nivo/bar";
+import React from 'react';
+import { ResponsiveBar } from '@nivo/bar'
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import Divider from '@material-ui/core/Divider';
 
-let data = [
-    {
-      "country": "AD",
-      "hot dog": 161,
-      "hot dogColor": "hsl(299, 70%, 50%)",
-      "burger": 17,
-      "burgerColor": "hsl(152, 70%, 50%)",
-      "sandwich": 91,
-      "sandwichColor": "hsl(177, 70%, 50%)",
-      "kebab": 3,
-      "kebabColor": "hsl(172, 70%, 50%)",
-      "fries": 135,
-      "friesColor": "hsl(171, 70%, 50%)",
-      "donut": 124,
-      "donutColor": "hsl(290, 70%, 50%)"
-    },
-    {
-      "country": "AE",
-      "hot dog": 91,
-      "hot dogColor": "hsl(281, 70%, 50%)",
-      "burger": 145,
-      "burgerColor": "hsl(275, 70%, 50%)",
-      "sandwich": 168,
-      "sandwichColor": "hsl(40, 70%, 50%)",
-      "kebab": 153,
-      "kebabColor": "hsl(304, 70%, 50%)",
-      "fries": 126,
-      "friesColor": "hsl(174, 70%, 50%)",
-      "donut": 189,
-      "donutColor": "hsl(142, 70%, 50%)"
-    },
-    {
-      "country": "AF",
-      "hot dog": 121,
-      "hot dogColor": "hsl(60, 70%, 50%)",
-      "burger": 112,
-      "burgerColor": "hsl(101, 70%, 50%)",
-      "sandwich": 83,
-      "sandwichColor": "hsl(1, 70%, 50%)",
-      "kebab": 34,
-      "kebabColor": "hsl(142, 70%, 50%)",
-      "fries": 123,
-      "friesColor": "hsl(74, 70%, 50%)",
-      "donut": 80,
-      "donutColor": "hsl(298, 70%, 50%)"
-    },
-    {
-      "country": "AG",
-      "hot dog": 138,
-      "hot dogColor": "hsl(356, 70%, 50%)",
-      "burger": 51,
-      "burgerColor": "hsl(77, 70%, 50%)",
-      "sandwich": 158,
-      "sandwichColor": "hsl(359, 70%, 50%)",
-      "kebab": 161,
-      "kebabColor": "hsl(176, 70%, 50%)",
-      "fries": 55,
-      "friesColor": "hsl(81, 70%, 50%)",
-      "donut": 81,
-      "donutColor": "hsl(306, 70%, 50%)"
-    },
-    {
-      "country": "AI",
-      "hot dog": 146,
-      "hot dogColor": "hsl(22, 70%, 50%)",
-      "burger": 36,
-      "burgerColor": "hsl(339, 70%, 50%)",
-      "sandwich": 175,
-      "sandwichColor": "hsl(282, 70%, 50%)",
-      "kebab": 106,
-      "kebabColor": "hsl(163, 70%, 50%)",
-      "fries": 185,
-      "friesColor": "hsl(105, 70%, 50%)",
-      "donut": 12,
-      "donutColor": "hsl(72, 70%, 50%)"
-    },
-    {
-      "country": "AL",
-      "hot dog": 115,
-      "hot dogColor": "hsl(323, 70%, 50%)",
-      "burger": 142,
-      "burgerColor": "hsl(232, 70%, 50%)",
-      "sandwich": 168,
-      "sandwichColor": "hsl(162, 70%, 50%)",
-      "kebab": 62,
-      "kebabColor": "hsl(33, 70%, 50%)",
-      "fries": 70,
-      "friesColor": "hsl(3, 70%, 50%)",
-      "donut": 15,
-      "donutColor": "hsl(84, 70%, 50%)"
-    },
-    {
-      "country": "AM",
-      "hot dog": 11,
-      "hot dogColor": "hsl(164, 70%, 50%)",
-      "burger": 95,
-      "burgerColor": "hsl(38, 70%, 50%)",
-      "sandwich": 115,
-      "sandwichColor": "hsl(25, 70%, 50%)",
-      "kebab": 38,
-      "kebabColor": "hsl(133, 70%, 50%)",
-      "fries": 164,
-      "friesColor": "hsl(87, 70%, 50%)",
-      "donut": 131,
-      "donutColor": "hsl(51, 70%, 50%)"
+
+class BarChart extends React.Component {
+    state = {
+        data: [{
+          "Names": "John Smith - jsmith@gmail.com",
+          "Coaching Capstone Experience": 50,
+          "Coaching Capstone ExperienceColor": "hsl(299, 70%, 50%)",
+          "Introduction to MedEd Scholarship": 161,
+          "Introduction to MedEd ScholarshipColor": "hsl(299, 70%, 50%)",
+          "Prepare and Present Teaching Topics": 161,
+          "Prepare and Present Teaching TopicsColor": "hsl(299, 70%, 50%)",
+          "Educational Service and Citizenship": 17,
+          "Educational Service and CitizenshipColor": "hsl(152, 70%, 50%)",
+          "UME, GME, or IPE": 91,
+          "UME, GME, or IPEColor": "hsl(177, 70%, 50%)",
+          "Faculty Development in Medical Education": 3,
+          "Faculty Development in Medical EducationColor": "hsl(172, 70%, 50%)",
+          "Active Medical Education Commitee Membership": 135,
+          "Active Medical Education Commitee MembershipColor": "hsl(171, 70%, 50%)",
+          "Opt-in and Biannual Review": 124,
+          "Opt-in and Biannual ReviewColor": "hsl(290, 70%, 50%)"
+        },
+        {
+          "Names": "Mike Smith - msmith@gmail.com",
+          "Coaching Capstone Experience": 50,
+          "Coaching Capstone ExperienceColor": "hsl(299, 70%, 50%)",
+          "Introduction to MedEd Scholarship": 161,
+          "Introduction to MedEd ScholarshipColor": "hsl(299, 70%, 50%)",
+          "Prepare and Present Teaching Topics": 91,
+          "Prepare and Present Teaching TopicsColor": "hsl(281, 70%, 50%)",
+          "Educational Service and Citizenship": 145,
+          "Educational Service and CitizenshipColor": "hsl(275, 70%, 50%)",
+          "UME, GME, or IPE": 168,
+          "UME, GME, or IPEColor": "hsl(40, 70%, 50%)",
+          "Faculty Development in Medical Education": 153,
+          "Faculty Development in Medical EducationColor": "hsl(304, 70%, 50%)",
+          "Active Medical Education Commitee Membership": 126,
+          "Active Medical Education Commitee MembershipColor": "hsl(174, 70%, 50%)",
+          "Opt-in and Biannual Review": 189,
+          "Opt-in and Biannual ReviewColor": "hsl(142, 70%, 50%)"
+        },
+        {
+          "Names": "Ted Smith - tsmith@gmail.com",
+          "Coaching Capstone Experience": 50,
+          "Coaching Capstone ExperienceColor": "hsl(299, 70%, 50%)",
+          "Introduction to MedEd Scholarship": 161,
+          "Introduction to MedEd ScholarshipColor": "hsl(299, 70%, 50%)",
+          "Prepare and Present Teaching Topics": 121,
+          "Prepare and Present Teaching TopicsColor": "hsl(60, 70%, 50%)",
+          "Educational Service and Citizenship": 112,
+          "Educational Service and CitizenshipColor": "hsl(101, 70%, 50%)",
+          "UME, GME, or IPE": 83,
+          "UME, GME, or IPEColor": "hsl(1, 70%, 50%)",
+          "Faculty Development in Medical Education": 34,
+          "Faculty Development in Medical EducationColor": "hsl(142, 70%, 50%)",
+          "Active Medical Education Commitee Membership": 123,
+          "Active Medical Education Commitee MembershipColor": "hsl(74, 70%, 50%)",
+          "Opt-in and Biannual Review": 80,
+          "Opt-in and Biannual ReviewColor": "hsl(298, 70%, 50%)"
+        },
+        {
+          "Names": "Sam Smith - ssmith@gmail.com",
+          "Coaching Capstone Experience": 50,
+          "Coaching Capstone ExperienceColor": "hsl(299, 70%, 50%)",
+          "Introduction to MedEd Scholarship": 161,
+          "Introduction to MedEd ScholarshipColor": "hsl(299, 70%, 50%)",
+          "Prepare and Present Teaching Topics": 138,
+          "Prepare and Present Teaching TopicsColor": "hsl(356, 70%, 50%)",
+          "Educational Service and Citizenship": 51,
+          "Educational Service and CitizenshipColor": "hsl(77, 70%, 50%)",
+          "UME, GME, or IPE": 158,
+          "UME, GME, or IPEColor": "hsl(359, 70%, 50%)",
+          "Faculty Development in Medical Education": 161,
+          "Faculty Development in Medical EducationColor": "hsl(176, 70%, 50%)",
+          "Active Medical Education Commitee Membership": 55,
+          "Active Medical Education Commitee MembershipColor": "hsl(81, 70%, 50%)",
+          "Opt-in and Biannual Review": 81,
+          "Opt-in and Biannual ReviewColor": "hsl(306, 70%, 50%)"
+        },
+        {
+          "Names": "Luke Smith - lsmith@gmail.com",
+          "Coaching Capstone Experience": 50,
+          "Coaching Capstone ExperienceColor": "hsl(299, 70%, 50%)",
+          "Introduction to MedEd Scholarship": 161,
+          "Introduction to MedEd ScholarshipColor": "hsl(299, 70%, 50%)",
+          "Prepare and Present Teaching Topics": 146,
+          "Prepare and Present Teaching TopicsColor": "hsl(22, 70%, 50%)",
+          "Educational Service and Citizenship": 36,
+          "Educational Service and CitizenshipColor": "hsl(339, 70%, 50%)",
+          "UME, GME, or IPE": 175,
+          "UME, GME, or IPEColor": "hsl(282, 70%, 50%)",
+          "Faculty Development in Medical Education": 106,
+          "Faculty Development in Medical EducationColor": "hsl(163, 70%, 50%)",
+          "Active Medical Education Commitee Membership": 185,
+          "Active Medical Education Commitee MembershipColor": "hsl(105, 70%, 50%)",
+          "Opt-in and Biannual Review": 12,
+          "Opt-in and Biannual ReviewColor": "hsl(72, 70%, 50%)"
+        },
+        {
+          "Names": "Xavier Smith - xsmith@gmail.com",
+          "Coaching Capstone Experience": 50,
+          "Coaching Capstone ExperienceColor": "hsl(299, 70%, 50%)",
+          "Introduction to MedEd Scholarship": 161,
+          "Introduction to MedEd ScholarshipColor": "hsl(299, 70%, 50%)",
+          "Prepare and Present Teaching Topics": 115,
+          "Prepare and Present Teaching TopicsColor": "hsl(323, 70%, 50%)",
+          "Educational Service and Citizenship": 142,
+          "Educational Service and CitizenshipColor": "hsl(232, 70%, 50%)",
+          "UME, GME, or IPE": 168,
+          "UME, GME, or IPEColor": "hsl(162, 70%, 50%)",
+          "Faculty Development in Medical Education": 62,
+          "Faculty Development in Medical EducationColor": "hsl(33, 70%, 50%)",
+          "Active Medical Education Commitee Membership": 70,
+          "Active Medical Education Commitee MembershipColor": "hsl(3, 70%, 50%)",
+          "Opt-in and Biannual Review": 15,
+          "Opt-in and Biannual ReviewColor": "hsl(84, 70%, 50%)"
+        },
+        {
+          "Names": "Nate Smith - nsmith@gmail.com",
+          "Coaching Capstone Experience": 50,
+          "Coaching Capstone ExperienceColor": "hsl(299, 70%, 50%)",
+          "Introduction to MedEd Scholarship": 161,
+          "Introduction to MedEd ScholarshipColor": "hsl(299, 70%, 50%)",
+          "Prepare and Present Teaching Topics": 11,
+          "Prepare and Present Teaching TopicsColor": "hsl(164, 70%, 50%)",
+          "Educational Service and Citizenship": 95,
+          "Educational Service and CitizenshipColor": "hsl(38, 70%, 50%)",
+          "UME, GME, or IPE": 115,
+          "UME, GME, or IPEColor": "hsl(25, 70%, 50%)",
+          "Faculty Development in Medical Education": 38,
+          "Faculty Development in Medical EducationColor": "hsl(133, 70%, 50%)",
+          "Active Medical Education Commitee Membership": 164,
+          "Active Medical Education Commitee MembershipColor": "hsl(87, 70%, 50%)",
+          "Opt-in and Biannual Review": 131,
+          "Opt-in and Biannual ReviewColor": "hsl(51, 70%, 50%)"
+        }
+        ]
     }
-  ];
+  
+    componentWillReceiveProps(props) {
+        console.log('props',props)
+    }
 
-data = data.sort((a, b) => a.value - b.value);
+    render() {
+        const {data} = this.state
+    return (
+        <div style={{height:600}}>
 
-const theme = {
-  labels: { text: { fontSize: 11 } },
-  axis: {
-    ticks: { text: { fontSize: 11, fill: "grey" } }
-  }
-};
-
-const App = () => (
-  <div
-    style={{ width: "90%", height: "15em", float: "left", overflowY: "auto" }}
-  >
-    <ResponsiveBar
-      height={150}
-      layout="horizontal"
-      margin={{
-        top: 5,
-        right: 5,
-        bottom: 5,
-        left: 35
+        <ResponsiveBar
+         data={data}
+         keys={[ 'Coaching Capstone Experience' , 'Introduction to MedEd Scholarship' , 'Prepare and Present Teaching Topics', 'Educational Service and Citizenship' , 'UME, GME, or IPE', 'Faculty Development in Medical Education', 'Active Medical Education Commitee Membership', 'Opt-in and Biannual Review' ]}
+         indexBy="Names"
+         margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
+         padding={0.3}
+         valueScale={{ type: 'linear' }}
+         indexScale={{ type: 'band', round: true }}
+         colors={{ scheme: 'nivo' }}
+        defs={[
+          {
+              id: 'dots',
+              type: 'patternDots',
+              background: 'inherit',
+              color: '#38bcb2',
+              size: 4,
+              padding: 1,
+              stagger: true
+          },
+          {
+              id: 'lines',
+              type: 'patternLines',
+              background: 'inherit',
+              color: '#eed312',
+              rotation: -45,
+              lineWidth: 6,
+              spacing: 10
+          }
+      ]}
+      fill={[
+          {
+              match: {
+                  id: 'Active Medical Education Commitee Membership'
+              },
+              id: 'dots'
+          },
+          {
+              match: {
+                  id: 'UME, GME, or IPE'
+              },
+              id: 'lines'
+          }
+      ]}
+      borderColor={{ from: 'color', modifiers: [ [ 'darker', 1.6 ] ] }}
+      axisTop={null}
+      axisRight={null}
+      axisBottom={{
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: 'Names',
+          legendPosition: 'middle',
+          legendOffset: 32
       }}
-      data={data}
-      indexBy={_index => _index.value + ""}
-      keys={["value"]}
-      colors={["#cce4f2"]}
-      colorBy="indexValue"
-      groupMode="grouped"
-      enableGridX={false}
-      enableGridY={false}
-      borderRadius={1}
-      label={_label => _label.data.id}
-      padding={0.2}
-      labelFormat={d => <tspan x={40}>{d}</tspan>}
       axisLeft={{
-        tickSize: 0,
-        tickPadding: 5,
-        tickRotation: 0
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: 'Percent complete',
+          legendPosition: 'middle',
+          legendOffset: -40
       }}
-      axisBottom={null}
-      labelTextColor="black"
-      motionStiffness={170}
-      motionDamping={26}
-      theme={theme}
-    />
-  </div>
-);
+      labelSkipWidth={12}
+      labelSkipHeight={12}
+      labelTextColor={{ from: 'color', modifiers: [ [ 'darker', 1.6 ] ] }}
+      legends={[
+          {
+              dataFrom: 'keys',
+              anchor: 'bottom-right',
+              direction: 'column',
+              justify: false,
+              translateX: 120,
+              translateY: 0,
+              itemsSpacing: 2,
+              itemWidth: 100,
+              itemHeight: 20,
+              itemDirection: 'left-to-right',
+              itemOpacity: 0.85,
+              symbolSize: 20,
+              effects: [
+                  {
+                      on: 'hover',
+                      style: {
+                          itemOpacity: 1
+                      }
+                  }
+              ]
+          }
+      ]}
+      animate={true}
+      motionStiffness={90}
+      motionDamping={15}
+  />
+  
+  <Button variant="contained" color="Default">
+  Email to fill out passport
+</Button>
+<Button variant="contained" color="Default">
+  Email that component is overdue
+</Button>
 
-render(<App />, document.getElementById("root"));
+    </div>
+    );
+  }
+}
+
+export default BarChart;
