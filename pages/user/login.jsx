28276725fd0e814d194c0bd285f7fbd379a0d263
BinBin He <hebinsquared@gmail.com>
2021-02-11 06:55:46 -0600

    Feature Update: Adding in sign in page + changed corresponding link to login.

---
+++ pages/user/login.jsx
@@ -0,0 +1,118 @@
+import Head from "next/head";
+import styles from "../../styles/Home.module.css";
+import Copyright from "../../components/Copyright";
+import Navbar from "../../components/Navbar";
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+      marginTop: theme.spacing(8),
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+      width: '100%', // Fix IE 11 issue.
+      marginTop: theme.spacing(3),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+    },
+  }));
+  
+
+export default function Login() {
+    const classes = useStyles();
+
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Teaching Passport</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Navbar />
+      <main className={styles.main}>
+        <Container component="main" maxWidth="xs">
+          <div className={classes.paper}>
+            <Typography component="h1" variant="h5">
+              Passport to Clinical Teaching
+            </Typography>
+            <form className={classes.form} noValidate>
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  <TextField
+                    variant="outlined"
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                    autoComplete="email"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    variant="outlined"
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    autoComplete="current-password"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <FormControlLabel
+                    control={
+                      <Checkbox value="allowExtraEmails" color="primary" />
+                    }
+                    label="Remember me"
+                  />
+                </Grid>
+              </Grid>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+              >
+                Log In
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="#" variant="body2">
+                    {"Don't have an account? Sign up"}
+                  </Link>
+                </Grid>
+              </Grid>
+            </form>
+          </div>
+        </Container>
+      </main>
+
+      <footer className={styles.footer}>
+        <Copyright />
+      </footer>
+    </div>
+  );
+}
