6ee93e90d698688437c25457bd3447896d5e9d71
BinBin He <hebinsquared@gmail.com>
2021-02-11 05:08:30 -0600

    Refactor: Ran prettier + removed unneeded code.

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,79 +1,120 @@
-import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Button from '@material-ui/core/Button';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Link from '@material-ui/core/Link';
-import { makeStyles } from '@material-ui/core/styles';
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Button from "@material-ui/core/Button";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
-    '@global': {
-      ul: {
-        margin: 0,
-        padding: 0,
-        listStyle: 'none',
-      },
+  "@global": {
+    ul: {
+      margin: 0,
+      padding: 0,
+      listStyle: "none",
     },
-    appBar: {
-      borderBottom: `1px solid ${theme.palette.divider}`,
+  },
+  appBar: {
+    borderBottom: `1px solid ${theme.palette.divider}`,
+  },
+  toolbar: {
+    flexWrap: "wrap",
+  },
+  toolbarTitle: {
+    flexGrow: 1,
+  },
+  nav: {
+    flex: 1,
+    align: "right",
+  },
+  link: {
+    margin: theme.spacing(1, 1.5),
+  },
+  heroContent: {
+    padding: theme.spacing(8, 0, 6),
+  },
+  cardHeader: {
+    backgroundColor:
+      theme.palette.type === "light"
+        ? theme.palette.grey[200]
+        : theme.palette.grey[700],
+  },
+  cardPricing: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "baseline",
+    marginBottom: theme.spacing(2),
+  },
+  footer: {
+    borderTop: `1px solid ${theme.palette.divider}`,
+    marginTop: theme.spacing(8),
+    paddingTop: theme.spacing(3),
+    paddingBottom: theme.spacing(3),
+    [theme.breakpoints.up("sm")]: {
+      paddingTop: theme.spacing(6),
+      paddingBottom: theme.spacing(6),
     },
-    toolbar: {
-      flexWrap: 'wrap',
-    },
-    toolbarTitle: {
-      flexGrow: 1,
-    },
-    link: {
-      margin: theme.spacing(1, 1.5),
-    },
-    heroContent: {
-      padding: theme.spacing(8, 0, 6),
-    },
-    cardHeader: {
-      backgroundColor:
-        theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[700],
-    },
-    cardPricing: {
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'baseline',
-      marginBottom: theme.spacing(2),
-    },
-    footer: {
-      borderTop: `1px solid ${theme.palette.divider}`,
-      marginTop: theme.spacing(8),
-      paddingTop: theme.spacing(3),
-      paddingBottom: theme.spacing(3),
-      [theme.breakpoints.up('sm')]: {
-        paddingTop: theme.spacing(6),
-        paddingBottom: theme.spacing(6),
-      },
-    },
-  }));
+  },
+}));
 
-function Navbar(){
-    const classes = useStyles();
+function Navbar() {
+  const classes = useStyles();
 
-    return(
-        <AppBar position="static" color="default" elevation={0} className={classes.appBar}>
-        <Toolbar className={classes.toolbar}>
-        <Typography variant="h6" color="inherit" noWrap className={classes.toolbarTitle}>
-            Passport To Clinical Teaching
-        </Typography>
-        <nav>
-            <Link variant="button" color="textPrimary" href="#" className={classes.link}>
-            About
-            </Link>
-            <Link variant="button" color="textPrimary" href="#" className={classes.link}>
-            Support
-            </Link>
-        </nav>
-        <Button href="#" color="primary" variant="outlined" className={classes.link}>
-            Login
-        </Button>
-        </Toolbar>
-        </AppBar>
-    );
+  return (
+    <AppBar
+      position="static"
+      color="default"
+      elevation={0}
+      className={classes.appBar}
+    >
+      <Toolbar className={classes.toolbar}>
+        <Grid container space={24} alightItems="center">
+          <Grid item>
+            <Typography
+              variant="h4"
+              color="inherit"
+              noWrap
+              className={classes.toolbarTitle}
+            >
+              Passport To Clinical Teaching
+            </Typography>
+          </Grid>
+
+          <Grid item xs container direction="row-reverse">
+            <Button
+              href="#"
+              color="primary"
+              variant="outlined"
+              className={classes.link}
+            >
+              Login
+            </Button>
+            <Grid item>
+              <Button
+                variant="button"
+                color="textPrimary"
+                href="#"
+                className={classes.link}
+              >
+                About
+              </Button>
+            </Grid>
+            <Grid item>
+              <Button
+                variant="button"
+                color="textPrimary"
+                href="#"
+                className={classes.link}
+              >
+                Support
+              </Button>
+            </Grid>
+            <Grid item></Grid>
+          </Grid>
+        </Grid>
+      </Toolbar>
+    </AppBar>
+  );
 }
 
 export default Navbar;
