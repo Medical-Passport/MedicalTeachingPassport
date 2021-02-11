be0816d0dba3052b503977eb5b03b55fb0fc48d9
BinBin He <hebinsquared@gmail.com>
2021-02-11 05:49:44 -0600

    Feature Update: Finalized prototype homepage design.

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-
+import Copyright from '../components/Copyright'
+import Navbar from '../components/Navbar'
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -9,56 +10,36 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+
+      <Navbar/>
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="#">your clinical passport!</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          A web-application for medical professionals to log activities designed to improve their clinical teaching skills.
         </p>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>example&rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <h3>Login&rarr;</h3>
+            <p>Login to track your current clinical teaching journey!</p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+            <h3>Register&rarr;</h3>
+            <p>Register a new account to track your clinical teaching journey!</p>
           </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+          <a href="https://nextjs.org/learn" className={styles.card}>
+            <h3>Learn&rarr;</h3>
+            <p>Check out our About page to answer some common questions!</p>
           </a>
         </div>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+      <Copyright/>
       </footer>
     </div>
   )
