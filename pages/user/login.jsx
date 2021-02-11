c44a7aa4deebf1e44b3d5a6a2a9f5b6b299ff5b9
BinBin He <hebinsquared@gmail.com>
2021-02-11 11:44:18 -0600

    Refactor: Fixed visual bug and size.

--- pages/user/login.jsx
+++ pages/user/login.jsx
@@ -2,39 +2,36 @@ import Head from "next/head";
 import styles from "../../styles/Home.module.css";
 import Copyright from "../../components/Copyright";
 import Navbar from "../../components/Navbar";
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+import React from "react";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
 
 const useStyles = makeStyles((theme) => ({
-    paper: {
-      marginTop: theme.spacing(8),
-    },
-    avatar: {
-      margin: theme.spacing(1),
-      backgroundColor: theme.palette.secondary.main,
-    },
-    form: {
-      width: '100%', // Fix IE 11 issue.
-      marginTop: theme.spacing(3),
-    },
-    submit: {
-      margin: theme.spacing(3, 0, 2),
-    },
-  }));
-  
+  paper: {
+    marginTop: theme.spacing(8),
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
 export default function Login() {
-    const classes = useStyles();
-
+  const classes = useStyles();
 
   return (
     <div className={styles.container}>
@@ -45,7 +42,7 @@ export default function Login() {
 
       <Navbar />
       <main className={styles.main}>
-        <Container component="main" maxWidth="xs">
+        <Container component="main" maxWidth="md">
           <div className={classes.paper}>
             <Typography component="h1" variant="h5">
               Passport to Clinical Teaching
@@ -54,7 +51,6 @@ export default function Login() {
               <Grid container spacing={2}>
                 <Grid item xs={12}>
                   <TextField
-                    variant="outlined"
                     required
                     fullWidth
                     id="email"
@@ -65,7 +61,6 @@ export default function Login() {
                 </Grid>
                 <Grid item xs={12}>
                   <TextField
-                    variant="outlined"
                     required
                     fullWidth
                     name="password"
@@ -75,12 +70,10 @@ export default function Login() {
                     autoComplete="current-password"
                   />
                 </Grid>
-                <Grid item xs={12}>
+                <Grid item xs>
                   <FormControlLabel
-                    control={
-                      <Checkbox value="allowExtraEmails" color="primary" />
-                    }
-                    label="Remember me"
+                    control={<Checkbox name="remember_login" color="primary" />}
+                    label="Remember Me"
                   />
                 </Grid>
               </Grid>
