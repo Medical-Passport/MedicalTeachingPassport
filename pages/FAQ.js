e5735b17da4af2d041c9f4b490ac4e96f5d8f898
Lingxing Zheng <xingling39@gmail.com>
2021-02-11 14:43:23 -0600

    Feature Update: Updating support email contact

--- pages/FAQ.js
+++ pages/FAQ.js
@@ -7,7 +7,7 @@ const FAQ = () => {
          <Typography variant='h4'>What is Medical Teaching Passport?</Typography>
          <Typography paragraph>This is a web-application for medical professionals to log activities designed to improve their clinical teaching skills. Participants will complete specific activities within certain medical education domains and record them in this tool so that passport program directors can monitor their progress and optimize mentoring.</Typography>
          <Typography variant='h4'>Contact Us</Typography>
-         <Link>test@example.com</Link>
+         <Link>support@passporttoclinicalteaching.com</Link>
       </Container>
    )
 }
