d5dd2d8a4d5fb5c661c2964655f47fa74ce32702
Lingxing Zheng <xingling39@gmail.com>
2021-03-20 21:27:36 -0500

    Feature: Adding contact page and email forwarding capabilities

--- pages/api/sendnotif.js
+++ pages/api/sendnotif.js
@@ -3,16 +3,19 @@ import sgMail from '@sendgrid/mail';
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 export default (req, res) => {
-   res.statusCode = 200
-
    // adjust content to accept details from response object
+   // console.log(req.body)
+   const body = JSON.parse(req.body)
+   
+   // modify text and html to include name and email
    let mailContent = {
       from: 'support@passporttoclinicalteaching.com',
       to: `${process.env.TEST_EMAIL}`,
-      subject: 'Testing Email Forwarding',
-      text: 'testing...',
-      html: '<p>testing...</p>'
+      subject: 'Support Ticket Submission',
+      text: `FROM: "${body.name}" <${body.email}>\nTIME: ${new Date().toLocaleString()}\n${body.body}`,
+      html: `<p>FROM: &quot;${body.name}&quot; &lt;${body.email}&gt;</p><p>TIME: ${new Date().toLocaleString()}</p><p>${body.body}</p>`
    }
+   console.log(mailContent)
 
    sgMail
       .send(mailContent)
@@ -20,7 +23,8 @@ export default (req, res) => {
          // console.log(`Sent!`);
       })
       .catch((err) => {
-         console.error(err);
+         // console.error(err);
       });
-   res.end();
+
+   res.status(200).send("Sent!");
 }
--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,3 +1,4 @@
+import {useRef} from 'react';
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Copyright from "../components/Copyright";
@@ -5,6 +6,8 @@ import Navbar from "../components/Navbar";
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
+import Container from '@material-ui/core/Container';
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles((theme) => ({
    paper: {
@@ -19,7 +22,27 @@ const useStyles = makeStyles((theme) => ({
 export default function Login() {
    const classes = useStyles();
 
-   // prefill email and name if the user is logged in
+   const sName = useRef();
+   const sEmail = useRef();
+   const sBody = useRef();
+
+   const sendTicket = () => {
+      fetch("/api/sendnotif", {
+         method: "POST",
+         body: JSON.stringify({
+            name: sName.current.value,
+            email: sEmail.current.value,
+            body: sBody.current.value,
+         })
+      }).then(res => {
+         // console.log("fetch Response!");
+      }).catch(err => {
+         // console.log("Oops!");
+      });
+   }
+
+   // prefill email and name if the user is logged in (QoL optional feature)
+   // Note: stupid thing happens when using outlined variant and refreshing page
    return (
       <div className={styles.container}>
          <Head>
@@ -28,26 +51,57 @@ export default function Login() {
          </Head>
 
          <Navbar />
+
          <main className={styles.main}>
-            <Grid container spacing={3} justify="space-between">
-               <Grid item xs={6}>
-                  <TextField id="support-name" label="Name" variant="filled" />
+            <Container component="main" maxWidth="md">
+               <Grid container justify="space-between" spacing={2}>
+                  <Grid item xs={11} sm={5}>
+                     <TextField
+                        id="support-name"
+                        label="Name"
+                        fullWidth
+                        // variant="filled"
+                        inputRef={sName}
+                     />
+                  </Grid>
+                  <Grid item xs={11} sm={5}>
+                     <TextField
+                        id="support-email"
+                        label="Email"
+                        fullWidth
+                        // variant="filled"
+                        inputRef={sEmail}
+                     />
+                  </Grid>
+                  <Grid item xs={12}>
+                     <TextField
+                        id="support-body"
+                        label="How can we help?"
+                        fullWidth
+                        multiline
+                        rows={4}
+                        // variant="outlined"
+                        variant="filled"
+                        inputRef={sBody}
+                     />
+                  </Grid>
                </Grid>
-               <Grid item xs={6}>
-                  <TextField id="support-email" label="Email" variant="filled" />
+               <br/>
+               <Grid container justify="flex-end">
+                  <Grid item xs={2}>
+                     <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        onClick={sendTicket}
+                     >
+                        Send
+                     </Button>
+                  </Grid>
                </Grid>
-               <Grid item xs={12}>
-                  <TextField
-                     fullWidth
-                     id="support-details"
-                     label="What do you need help with?"
-                     multiline
-                     rows={5}
-                     variant="outlined"
-                  />
-               </Grid>
-               
-            </Grid>
+            </Container>
+
          </main>
          <footer className={styles.footer}>
             <Copyright />
