5cc2ffbb85bc5f11fafa9a19a1e3a64e13200d1f
Lingxing Zheng <xingling39@gmail.com>
2021-03-22 12:51:59 -0500

    Issue: Fixing function naming

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -19,7 +19,7 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-export default function Login() {
+export default function Contact() {
    const classes = useStyles();
 
    const sName = useRef();
